d41e01cefe9fb756f987de1058ad29d121247f79
maberchtold <berchtold.matteo@gmail.com>
2023-12-03 00:04:55 +0100

    exercise string 2 1

---
+++ src/w11/AufgabenString_2_1.java
@@ -0,0 +1,19 @@
+package w11;
+
+import java.util.Scanner;
+
+public class AufgabenString_2_1 {
+    public static void main(String[] args) {
+        String userInput = "";
+        String[] stringArr;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Geben Sie einen Satz ein: ");
+        userInput = scanner.nextLine();
+
+        stringArr = userInput.split(" ");
+
+        System.out.println("Der Satz besteht aus "+stringArr.length+" Wörter.");
+    }
+}
