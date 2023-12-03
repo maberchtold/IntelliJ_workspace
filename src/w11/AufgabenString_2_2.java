dad8046093348fa76a3df7bf51e63d0a2c073b45
maberchtold <berchtold.matteo@gmail.com>
2023-12-03 10:07:16 +0100

    exercise string 2_2

---
+++ src/w11/AufgabenString_2_2.java
@@ -0,0 +1,34 @@
+package w11;
+
+import java.util.Scanner;
+
+public class AufgabenString_2_2 {
+    public static void main(String[] args) {
+        String[] wordArr = new String[10];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < wordArr.length; i++) {
+            System.out.print("Geben Sie das "+(i+1)+". Wort ein: ");
+            wordArr[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i < wordArr.length; i++) {
+            if (hasDoubleConsonant(wordArr[i])){
+                System.out.println(wordArr[i]);
+            }
+        }
+    }
+
+    public static boolean hasDoubleConsonant(String word){
+        boolean doubleConsonant = false;
+
+        for (int i = 0; i < word.length() - 1; i++){
+            if (word.charAt(i) == word.charAt(i+1)){
+                doubleConsonant = true;
+            }
+        }
+
+        return doubleConsonant;
+    }
+}
